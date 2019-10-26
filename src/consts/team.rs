fdd1b1516ce0e2a1f16f1fa2af25bc5d7c8ed9b9
Mingwei Samuel <mingwei.samuel@gmail.com>
2019-10-25 19:54:29 -0700

    update team enum

--- src/consts/team.rs
+++ src/consts/team.rs
@@ -1,4 +1,9 @@
+#[derive(Debug, Copy, Clone)]
+#[derive(Eq, PartialEq, Hash, Ord, PartialOrd)]
+/// League of Legends team.
 pub enum Team {
+    /// Blue team (bottom left on Summoner's Rift).
     Blue = 100,
+    /// Red team (top right on Summoner's Rift).
     Red = 200,
 }
