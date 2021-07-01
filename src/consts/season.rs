4b23f9afed1e9760c3eb94d192d839e6d3bf25c1
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-06-30 19:46:26 -0700

    Convert Season from enum into newtype_enum

--- src/consts/season.rs
+++ src/consts/season.rs
@@ -6,6 +6,30 @@
 //                                           //
 ///////////////////////////////////////////////
 
+newtype_enum! {
+    /// A League of Legends season for competitive matchmaking.
+    #[derive(Serialize, Deserialize)]
+    #[serde(transparent)]
+    pub newtype_enum Season {
+        PRESEASON_3 = 0,
+        SEASON_3 = 1,
+        PRESEASON_2014 = 2,
+        SEASON_2014 = 3,
+        PRESEASON_2015 = 4,
+        SEASON_2015 = 5,
+        PRESEASON_2016 = 6,
+        SEASON_2016 = 7,
+        PRESEASON_2017 = 8,
+        SEASON_2017 = 9,
+        PRESEASON_2018 = 10,
+        SEASON_2018 = 11,
+        PRESEASON_2019 = 12,
+        SEASON_2019 = 13,
+        PRESEASON_2020 = 14,
+        SEASON_2020 = 15,
+    }
+}
+
 use serde_repr::{ Serialize_repr, Deserialize_repr };
 use num_enum::{ IntoPrimitive, TryFromPrimitive };
 
@@ -16,7 +40,7 @@ use num_enum::{ IntoPrimitive, TryFromPrimitive };
 #[derive(Serialize_repr, Deserialize_repr)]
 #[derive(IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
-pub enum Season {
+pub enum Season2 {
     PRESEASON_3 = 0,
     SEASON_3 = 1,
     PRESEASON_2014 = 2,
