f1821916801d2167254ad12c13c751a8517d3c7e
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-04 10:04:44 -0700

    Only check stable for compilation in test.bash

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cargo +stable test -- --nocapture
-RGAPI_KEY="$(cat apikey.txt)" RUST_BACKTRACE=1 RUST_LOG=riven=debug cargo +nightly test --features nightly -- --nocapture
+cargo +stable test --no-run
+RGAPI_KEY="$(cat apikey.txt)" RUST_BACKTRACE=1 RUST_LOG=riven=trace cargo +nightly test --features nightly -- --nocapture
