ad1b4fd630c166ef6f5093d8e062573380936f73
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-10-07 09:24:56 -0700

    Add Team ID 300 for baron killing herald

--- riven/src/consts/team.rs
+++ riven/src/consts/team.rs
@@ -6,10 +6,13 @@ use num_enum::{ IntoPrimitive, TryFromPrimitive };
 #[derive(Eq, PartialEq, Hash, Ord, PartialOrd)]
 #[derive(Serialize_repr, Deserialize_repr)]
 #[derive(IntoPrimitive, TryFromPrimitive)]
-#[repr(u8)]
+#[repr(u16)]
 pub enum Team {
     /// Blue team (bottom left on Summoner's Rift).
     BLUE = 100,
     /// Red team (top right on Summoner's Rift).
     RED = 200,
+
+    /// "killerTeamId" when Baron Nashor spawns and kills Rift Herald.
+    OTHER = 300,
 }
