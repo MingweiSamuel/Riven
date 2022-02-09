62f323da88c4e51d2197ffb1f0085be784c9e877
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-02-08 17:18:16 -0800

    Release 2.5.1

--- riven/tests/tests_euw.rs
+++ riven/tests/tests_euw.rs
@@ -32,24 +32,27 @@ async_tests!{
             rassert!(s.len() >= 142, "Expected masteries: {}.", s.len());
             Ok(())
         },
-        spectator_combo: async {
-            let featured_p = RIOT_API.spectator_v4().get_featured_games(ROUTE);
-            let featured = featured_p.await.map_err(|e| e.to_string())?;
 
-            rassert!(!featured.game_list.is_empty());
+        // TODO: https://github.com/RiotGames/developer-relations/issues/602
+        // spectator_combo: async {
+        //     let featured_p = RIOT_API.spectator_v4().get_featured_games(ROUTE);
+        //     let featured = featured_p.await.map_err(|e| e.to_string())?;
 
-            let summoner_name = &featured.game_list[0].participants[0].summoner_name;
-            let summoner_p = RIOT_API.summoner_v4().get_by_summoner_name(ROUTE, summoner_name);
-            let summoner = summoner_p.await.map_err(|e| e.to_string())?.ok_or_else(|| "Failed to get summoner".to_owned())?;
+        //     rassert!(!featured.game_list.is_empty());
 
-            let livegame_p = RIOT_API.spectator_v4().get_current_game_info_by_summoner(ROUTE, &summoner.id);
-            let livegame_o = livegame_p.await.map_err(|e| e.to_string())?;
-            if let Some(livegame) = livegame_o {
-                let participant_match = livegame.participants.iter().find(|p| p.summoner_name == *summoner_name);
-                rassert!(participant_match.is_some(), "Failed to find summoner in match: {}.", summoner_name);
-            }
-            Ok(())
-        },
+        //     let summoner_name = &featured.game_list[0].participants[0].summoner_name;
+        //     let summoner_p = RIOT_API.summoner_v4().get_by_summoner_name(ROUTE, summoner_name);
+        //     let summoner = summoner_p.await.map_err(|e| e.to_string())?.ok_or_else(|| "Failed to get summoner".to_owned())?;
+
+        //     let livegame_p = RIOT_API.spectator_v4().get_current_game_info_by_summoner(ROUTE, &summoner.id);
+        //     let livegame_o = livegame_p.await.map_err(|e| e.to_string())?;
+        //     if let Some(livegame) = livegame_o {
+        //         let participant_match = livegame.participants.iter().find(|p| p.summoner_name == *summoner_name);
+        //         rassert!(participant_match.is_some(), "Failed to find summoner in match: {}.", summoner_name);
+        //     }
+        //     Ok(())
+        // },
+
         // // TFT tests.
         // tftleaguev1_getchallengerleague: async {
         //     let p = RIOT_API.tft_league_v1().get_challenger_league(Region::EUW);
