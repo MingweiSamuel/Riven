0753fe1b3406084f83d1f7f9a5a5f34c0376b8f4
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-01-07 18:09:11 -0800

    Update `league_getforsummoner_tftbug` test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-RGAPI_KEY="$(cat apikey.txt)" RUST_BACKTRACE=1 RUST_LOG=riven=trace cargo test --features nightly,deny-unknown -- --nocapture
+RGAPI_KEY="$(cat apikey.txt)" RUST_BACKTRACE=1 RUST_LOG=riven=debug cargo test --features nightly,deny-unknown -- --nocapture
