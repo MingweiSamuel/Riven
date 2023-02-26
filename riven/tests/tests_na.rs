129002bf23b7727ab6e39a9d21465be1ea9f56f3
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-02-26 11:43:37 -0800

    Fix tft doubleup test again

--- riven/tests/tests_na.rs
+++ riven/tests/tests_na.rs
@@ -51,7 +51,8 @@ async_tests!{
         // TO TEST THIS BUG: https://github.com/RiotGames/developer-relations/issues/572.
         // summoner must have double-up rank.
         league_getforsummoner_tftbug: async {
-            const SUMMONER_NAME: &'static str = "rebocca";
+            // TODO(mingwei): get summoner from leaderboard to avoid updating this all the time.
+            const SUMMONER_NAME: &'static str = "VX 18549996114";
             let summoner_fut = RIOT_API.summoner_v4().get_by_summoner_name(ROUTE, SUMMONER_NAME);
             let summoner = summoner_fut.await.map_err(|e| e.to_string())?.ok_or_else(|| format!("Failed to get \"{}\"", SUMMONER_NAME))?;
             let league_fut = RIOT_API.league_v4().get_league_entries_for_summoner(ROUTE, &*summoner.id);
