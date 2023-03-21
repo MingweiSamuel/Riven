d05b068bb8fb12a8a8ce20b296ea75b389c5c241
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-03-20 21:01:41 -0700

    Remove unused key-specific IDs in testutils

--- riven/tests/testutils.rs
+++ riven/tests/testutils.rs
@@ -15,14 +15,6 @@ lazy_static! {
     };
 }
 
-pub mod ids {
-    pub const SUMMONER_ID_LUGNUTSK: &str = "SBM8Ubipo4ge2yj7bhEzL7yvV0C9Oc1XA2l6v5okGMA_nCw";
-    pub const SUMMONER_ID_MA5TERY: &str = "IbC4uyFEEW3ZkZw6FZF4bViw3P1EynclAcI6-p-vCpI99Ec";
-    pub const SUMMONER_ID_C9SNEAKY: &str = "ghHSdADqgxKwcRl_vWndx6wKiyZx0xKQv-LOhOcU5LU";
-    pub const ACCOUNT_ID_C9SNEAKY: &str = "ML_CcLT94UUHp1iDvXOXCidfmzzPrk_Jbub1f_INhw";
-    pub const ACCOUNT_ID_LUGNUTSK: &str = "iheZF2uJ50S84Hfq6Ob8GNlJAUmBmac-EsEEWBJjD01q1jQ";
-}
-
 pub async fn tft_match_v1_get(route: RegionalRoute, matches: &[impl AsRef<str>]) -> Result<(), String> {
     let futures = matches.iter().map(|matche| async move {
         let matche = matche.as_ref();
