2c284d860cbe44b83a069e17cd6e4a67b0993809
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-01-15 11:34:10 -0800

    test.bash RUST_BACKTRACE=full

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-RGAPI_KEY="$(cat apikey.txt)" RUST_BACKTRACE=1 RUST_LOG=riven=debug cargo test --no-fail-fast --features nightly,deny-unknown -- --nocapture
+RGAPI_KEY="$(cat apikey.txt)" RUST_BACKTRACE=full RUST_LOG=riven=debug cargo test --no-fail-fast --features nightly,deny-unknown -- --nocapture
