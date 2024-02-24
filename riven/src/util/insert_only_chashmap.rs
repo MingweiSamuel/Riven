4faa8e83e4bb52cd0b0d037fed74fd6a6809e867
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-02-24 00:27:29 -0800

    style: cleanup `insert_only_chashmap.rs` (#63)

--- riven/src/util/insert_only_chashmap.rs
+++ riven/src/util/insert_only_chashmap.rs
@@ -1,4 +1,3 @@
-// use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::sync::Arc;
@@ -16,15 +15,6 @@ impl<K: Hash + Eq, V> InsertOnlyCHashMap<K, V> {
             base: Mutex::new(HashMap::new()),
         }
     }
-
-    // #[inline]
-    // pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<Arc<V>>
-    // where
-    //     K: Borrow<Q>,
-    //     Q: Hash + Eq,
-    // {
-    //     self.base.lock().get(key).map(|v| Arc::clone(v))
-    // }
 
     #[inline]
     pub fn get_or_insert_with<F: FnOnce() -> V>(&self, key: K, default: F) -> Arc<V> {
