9e8f3449c66394285c9e7fc8311876ad69b56a4c
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-02-24 20:48:40 -0800

    test: add testing and ci for wasm (#63)

--- test-full.bash
+++ test-full.bash
@@ -2,12 +2,15 @@
 set -euxo pipefail
 
 # Ensure stable builds.
-cargo +stable check --all-targets
-cargo +stable check --all-targets --features tracing
+cargo +stable check --all-targets --features __proxy
+cargo +stable check --all-targets --features tracing,__proxy
 
 # Ensure nightly builds.
-cargo check --all-targets --features nightly,tracing
-cargo build --all-targets --features nightly,deny-unknown
+cargo check --all-targets --features nightly,tracing,__proxy
+cargo build --all-targets --features nightly,deny-unknown,__proxy
 
 # Run nightly tests.
 bash test.bash
+
+# Run wasm tests.
+bash test-wasm.bash
---
+++ test-wasm.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+export RGAPI_KEY="$(cat apikey.txt)"
+
+cd riven
+
+# Ensure builds with tracing.
+wasm-pack build -- --features nightly,tracing
+
+# Run tests.
+wasm-pack test --node -- --no-fail-fast --features nightly,deny-unknown
