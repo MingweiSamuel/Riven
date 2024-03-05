ff6a3af9e190aa1d1c5e30e4afa23cff9e2fb073
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-03-05 10:07:19 -0800

    test: stop print spamming platform data tests

--- riven/tests/tests_na.rs
+++ riven/tests/tests_na.rs
@@ -122,6 +122,6 @@ async fn clash_get_team_by_id_invalid() -> Result<(), String> {
 async fn status() -> Result<(), String> {
     let p = riot_api().lol_status_v4().get_platform_data(ROUTE);
     let status = p.await.map_err(|e| e.to_string())?;
-    println!("{:?}", status);
+    let _ = status;
     Ok(())
 }
--- riven/tests/tests_ph2.rs
+++ riven/tests/tests_ph2.rs
@@ -8,6 +8,6 @@ const ROUTE: PlatformRoute = PlatformRoute::PH2;
 async fn status() -> Result<(), String> {
     let p = riot_api().lol_status_v4().get_platform_data(ROUTE);
     let status = p.await.map_err(|e| e.to_string())?;
-    println!("{:?}", status);
+    let _ = status;
     Ok(())
 }
