1cbd3d965cd380a5d7bef6f64991782257110950
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-04-10 10:52:03 -0700

    test: remove archived match `EUW1_6349186754`

--- riven/tests/tests_europe.rs
+++ riven/tests/tests_europe.rs
@@ -10,9 +10,9 @@ const ROUTE: RegionalRoute = RegionalRoute::EUROPE;
 // "EUW1_5097963383",
 // "EUW1_5102203800", // https://github.com/MingweiSamuel/Riven/issues/36
 // "EUW1_5765650307", // https://gist.github.com/MingweiSamuel/d5f9dc40cc5a80a9255e488f27705c56?permalink_comment_id=4088256#gistcomment-4088256
+// "EUW1_6349186754", // https://github.com/MingweiSamuel/Riven/issues/71
 
 static MATCHES: &[&str] = &[
-    "EUW1_6349186754", // https://github.com/MingweiSamuel/Riven/issues/71
     // New ARENA 2v2v2v2 game mode
     "EUW1_6511808246", // https://github.com/MingweiSamuel/Camille/issues/99
     // Added 2023-08-27
